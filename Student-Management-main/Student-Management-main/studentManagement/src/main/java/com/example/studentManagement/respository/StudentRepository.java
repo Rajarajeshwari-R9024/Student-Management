e5a0bf9260c7b81659a38d83daad1cbadbf6e9b2
Rajarajeshwari R <rajikishor9024@gmail.com>
2025-02-11 23:06:25 +0530

    Delete Student-Management-main/Student-Management-main directory

--- Student-Management-main/Student-Management-main/studentManagement/src/main/java/com/example/studentManagement/respository/StudentRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.studentManagement.respository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.studentManagement.entity.Student;
-public interface StudentRepository extends JpaRepository<Student, Long>{
-
-}
